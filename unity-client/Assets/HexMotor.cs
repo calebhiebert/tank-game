864b36a345899fdd89abb1b6d0e75bc881210078
Caleb Hiebert <calebkhiebert@gmail.com>
2017-05-08 09:50:36 -0500

    clean up

---
+++ unity-client/Assets/HexMotor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMotor : MonoBehaviour {
+
+    public Rigidbody2D body;
+    public float force = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector2.left * force);
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector2.right * force);
+        }
+    }
+}
